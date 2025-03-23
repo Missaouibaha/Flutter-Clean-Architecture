b734cd5ca245bde9862fe430b0f45e82fef7e93c
MissaouiBaha <lmissaouibaha123@gmail.com>
2025-03-24 00:09:12 +0100

    Set up networking module : - Dio Client & inerceptors - Retrofit - Api Result Model & handle errors Add setting.json file to exclude  generated and freezed files

---
+++ lib/core/networking/dio_factory.dart
@@ -0,0 +1,42 @@
+import 'package:clean_arch_app/core/helper/extensions.dart';
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  DioFactory._();
+
+  static Dio? dio;
+
+  static getDio() {
+    Duration timeOut = Duration(seconds: 25);
+    if (dio == null) {
+      dio = Dio();
+      dio?.let((it) {
+        it
+          ..options.connectTimeout = timeOut
+          ..options.receiveTimeout = timeOut;
+      });
+
+      addDioHeader();
+      addDioInterceptor();
+      return dio;
+    } else {
+      return dio;
+    }
+  }
+
+  static void addDioHeader() {
+    dio?.options.headers = {'Accept': 'application/json'};
+  }
+
+  static void addDioInterceptor() {
+    dio?.interceptors.add(
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+        responseBody: true,
+      ),
+    );
+  }
+}
